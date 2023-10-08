beb858f0906f26219f8731d11360bdbfa4d9c059
Arbaz <101106144+Arbazabbas@users.noreply.github.com>
2023-10-08 17:20:11 +0530

    Time: 3 ms (96.52%), Space: 7.9 MB (34.27%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -7,30 +7,64 @@
  * };
  */
 class Solution {
+   /* private:
+    ListNode* detectLoop(ListNode* &head){
+        if(head==NULL){
+            return NULL;
+        }
+        if(head->next==NULL){
+            return NULL;
+        }
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(slow!=NULL && fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast->next;
+            }
+            slow=slow->next;
+            if(slow==fast){
+                return slow;
+            }
+        }
+        return NULL;
+    }
+    public:
+    ListNode* getLoopNode(ListNode* &head){
+        if(head==NULL){
+            return NULL;
+        }
+        ListNode* intersection = detectLoop(head);
+        if(intersection==NULL){
+            return NULL;
+        }
+        ListNode*slow=head;
+        while(slow!=intersection){
+            slow=slow->next;
+            intersection=intersection->next;
+        }
+        return slow;
+    }*/
 public:
     ListNode *detectCycle(ListNode *head) {
-          ListNode* slow = head;
-    ListNode* fast = head;
-
-    
-    while (slow != nullptr && fast != nullptr && fast->next != nullptr) {
-        slow = slow->next;
-        fast = fast->next->next;
-
-       
-        if (slow == fast) {
-            fast = head;  
-
-        
-            while (fast != slow) {
-                fast = fast->next;
-                slow = slow->next;
-            }
-
-            return fast;  
+        if(head==NULL){
+            return NULL;
         }
-    }
-
-    return nullptr;  
+      //  return getLoopNode(head);
+        ListNode*fast=head;
+        ListNode*slow=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast){
+                slow=head;
+                while(fast!=slow){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
     }
 };
